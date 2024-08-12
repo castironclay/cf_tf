ea719d3bb973d63b6cd2a2ed02f29abad4fe3b4a
Clay Coppage <clay@horizon3.ai>
2024-08-12 17:47:29 -0400

    some updates

--- githubmesh.py
+++ githubmesh.py
@@ -1,6 +1,8 @@
 import yaml
 from time import sleep
 from httpx import Client
+import io
+import zipfile
 
 
 def read_creds_file():
@@ -62,16 +64,51 @@ class Workflow:
         workflow_id = workflow_details.get("workflow_runs")[0].get("id")
         get_logs = f"https://api.github.com/repos/{details.get("account")}/{details.get("repo")}/actions/runs/{workflow_id}/logs"
         s = self.session
-        logs = s.get(get_logs, follow_redirects=True)
-        download_url = logs.request.url
 
-        # Build fresh client without any special headers and download our logs zip
-        # A pre-signed URL is used so we dont need to worry about authentication
-        new_client = Client()
-        download_file = new_client.get(download_url)
-        # Write the binary response content to a file
-        with open("response.zip", "wb") as file:
-            file.write(download_file.content)
+        # Seems like it takes a bit for the logs to be available so this will try until we get a 200
+        cloudflared_url = False
+        status_code = 404
+        while status_code != 200:
+            logs = s.get(get_logs, follow_redirects=True)
+            status_code = logs.status_code
+            print("Not 200 yet")
+            sleep(3)
+
+        while not cloudflared_url:
+            download_url = logs.request.url
+            # Build fresh client without any special headers and download our logs zip
+            # A pre-signed URL is used so we dont need to worry about authentication
+            new_client = Client()
+            download_file = new_client.get(download_url)
+
+            print("checking logs")
+            content = download_file.content
+            try:
+                zip_file_io = io.BytesIO(content)
+
+                # Open the zip file from the BytesIO object
+                with zipfile.ZipFile(zip_file_io) as zf:
+                    # Iterate through the files in the zip archive
+                    for file_name in zf.namelist():
+                        # Read the file content into memory
+                        if "cloudflared" in file_name:
+                            with zf.open(file_name) as file:
+                                file_content = file.readlines()
+                                for line in file_content:
+                                    line = line.decode()
+                                    if ".trycloudflare.com" in line:
+                                        print(
+                                            f'Your URL is: {line.split("|")[1].strip()}'
+                                        )
+                                        cloudflared_url = True
+
+            except zipfile.BadZipFile:
+                # Will hit this error if we haven't gotten our zip file yet
+                sleep(3)
+                print("zip error")
+                continue
+
+            sleep(3)
 
 
 if __name__ == "__main__":
@@ -82,8 +119,11 @@ if __name__ == "__main__":
         work.start_workflow()
         work.check_running()
         print("workflow started")
-        sleep(5)
-        work.cancel_workflow()
-        print("workflow cancelled")
-        sleep(5)
+        # Sleep for 10 seconds before getting logs
+        # Could maybe grab logs repeatedly until we get the ones we want
+        sleep(10)
         work.get_workflow_logs()
+
+        sleep(1)
+        print("workflow cancelled")
+        work.cancel_workflow()
